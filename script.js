59656367acf5b705366fb04e9090531f45edbbf5
Rupak2580 <45221894+Rupak2580@users.noreply.github.com>
2020-09-01 19:41:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+var selectedRow = null
+
+function onFormSubmit() {
+        var formData = readFormData();
+        if (selectedRow == null)
+            insertNewRecord(formData);
+        else
+            updateRecord(formData);
+        resetForm();
+}
+
+function readFormData() {
+    var formData = {};
+    formData["fullName"] = document.getElementById("fullName").value;
+    formData["department"] = document.getElementById("department").value;
+    formData["roll"] = document.getElementById("roll").value;
+    return formData;
+}
+
+function insertNewRecord(data) {
+    var table = document.getElementById("studentList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.fullName;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.department;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.roll;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+                       <a onClick="onDelete(this)">Delete</a>`;
+}
+
+function resetForm() {
+    document.getElementById("fullName").value = "";
+    document.getElementById("department").value = "";
+    document.getElementById("roll").value = "";
+    selectedRow = null;
+}
+
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("department").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("roll").value = selectedRow.cells[2].innerHTML;
+    }
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.fullName;
+    selectedRow.cells[1].innerHTML = formData.department;
+    selectedRow.cells[2].innerHTML = formData.roll;
+    
+}
+
+function onDelete(td) {
+    if (confirm('Are you sure to delete this record ?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById("studentList").deleteRow(row.rowIndex);
+        resetForm();
+    }
+}
